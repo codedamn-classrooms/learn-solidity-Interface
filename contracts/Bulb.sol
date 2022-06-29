4e44606f5dcb0ad27ac0e210e04af21f705ef1f8
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-06-29 18:05:38 +0530

    initial

---
+++ contracts/Bulb.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+interface ISwitch {
+    function switchOn() external returns (uint256);
+
+    function switchOff() external returns (uint256);
+}
+
+contract Bulb is ISwitch {
+    function switchOn() public pure override returns (uint256) {
+        return 1;
+    }
+
+    function switchOff() public pure override returns (uint256) {
+        return 0;
+    }
+}
